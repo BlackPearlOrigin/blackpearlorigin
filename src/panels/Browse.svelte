93b2d360db560bbb50d7060654cba54c418e5a8f
Davi Santos <brisolo32@outlook.com>
2022-12-15 17:20:18 -0300

    Modified a lot of stuff
    Changed some styles
    Almost finished the Browser page
    Changed the config path
    Made a script to read plugins on the config path

--- src/panels/Browse.svelte
+++ src/panels/Browse.svelte
@@ -1,26 +1,42 @@
+<script lang="ts">
+    import { browse } from "./Browse"
+
+    let inputText: string;
+    let selectedScraper: string;
+
+    const data = browse()
+</script>
+
 <main class="container">
     <div class="main">
-        <h1 class="browse">Browse</h1>
+        <h1 style="text-align:left">Browse</h1>
 
-        <form class="search" action="/">
-            <input type="text">
-            <button type="submit">
+        <div class="search">
+            <input placeholder="Search" type="text" bind:value={inputText}>
+            <select bind:value={selectedScraper} name="Plugins">
+                <option value="Select">Select a plugin</option>
+                {#await data}
+                    {void(0)}
+                {:then d}
+                    {#each d.scrapers as Scrapers }
+                        <option value="{Scrapers.location}">{Scrapers.name}</option>
+                    {/each}
+                {/await}
+            </select>
+            <button type="submit" on:click={void(0)}>
                 <i class="fa-solid fa-magnifying-glass"></i>
             </button>
-        </form>
+        </div>
     </div>
 </main>
 
 <style>
-    .browse {
-        text-align: left;
-    }
-
     .search {
         display: flex;
     }
 
     .search > input {
+        font-size: 1rem;
         color: white;
         background-color: rgb(41, 41, 41);
         border-color: #393939;
@@ -46,6 +62,14 @@
         border-radius: 3px;
 
         cursor: pointer;
+    }
+
+    .search > select {
+        margin-left: 8px;
+        border-radius: 5px;
+
+        border-color: #393939;
+        border-width: 4px;
     }
 
     .search > button > i {

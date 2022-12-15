c5ee305c3ce76449d2e11f901288c72fa74f230c
zun1uwu <101455048+zun1uwu@users.noreply.github.com>
2022-12-15 14:24:15 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Greet from './lib/Greet.svelte'
+</script>
+
+<main class="container">
+  <h1>Welcome to Tauri!</h1>
+
+  <div class="row">
+    <Greet />
+  </div>
+
+</main>

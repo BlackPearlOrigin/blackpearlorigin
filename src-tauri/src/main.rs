c72482970cf3477662f96f0d674c10b76a7a746c
zun1uwu <zuni0@protonmail.com>
2022-12-15 16:45:44 +0100

    Base for implementing multiple panels

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,45 @@
     windows_subsystem = "windows"
 )]
 
+use std::{path::Path, fs};
+
 fn main() {
+    
+    create_paths();
+
     tauri::Builder::default()
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+fn create_paths() {
+
+    let username = whoami::username();
+
+    let local_path = match whoami::platform() {
+        whoami::Platform::Windows => Path::new(r"C:\").join("Users").join(format!("{}", username)).join("AppData").join("Local").join("Project Black Pearl"),
+        whoami::Platform::Linux => Path::new(r"/home").join(format!("{}", username)).join(".local").join("Project Black Pearl"),
+        whoami::Platform::MacOS => Path::new(r"/Users").join(format!("{}", username)).join(".local").join("Project Black Pearl"),
+        _ => Path::new("").to_path_buf(),
+    };
+
+    let temp_folder_path = local_path.join("temp");
+
+    if !local_path.exists() {
+        create(&local_path)
+    }
+    if !temp_folder_path.exists() {
+        create(&temp_folder_path)
+    }
+
+    fn create(path: &Path) {
+        match fs::create_dir_all(&path) {
+            Ok(k) => {
+                println!("Successfully created folder {}", &path.display());
+                k
+            },
+            Err(e) => eprintln!("Error while creating folder {}: {}\n Your data will not be saved.", &path.display(), e),
+        }
+    }
+}
+
--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,44 @@
 <script lang="ts">
-  import Greet from './lib/Greet.svelte'
+  
+  function switch_panels(panel: number) {
+
+    
+    
+    switch (panel) {
+      case 0:
+        console.log("Home");
+        break;
+
+      case 1:
+        console.log("Library");
+        break;
+
+      case 2:
+        console.log("Browse");
+        break;
+
+      case 3:
+        console.log("Downloads");
+        break;
+
+      case 4:
+        console.log("Preferences");
+        break;
+    }
+
+    return 0;
+  }
+
 </script>
 
 <main class="container">
   
   <div class="sidenav">
-    <a href="#">Home</a>
-    <a href="#">Library</a>
-    <a href="#">Browse</a>
-    <a href="#">Downloads</a>
-    <a href="#">Preferences</a>
+    <button id="home" class="menu_button" on:click={() => switch_panels(0)}>Home</button>
+    <button id="library" class="menu_button" on:click={() => switch_panels(1)}>Library</button>
+    <button id="browse" class="menu_button" on:click={() => switch_panels(2)}>Browse</button>
+    <button id="downloads" class="menu_button" on:click={() => switch_panels(3)}>Downloads</button>
+    <button id="preferences" class="menu_button" on:click={() => switch_panels(4)}>Preferences</button>
   </div>
 
 </main>

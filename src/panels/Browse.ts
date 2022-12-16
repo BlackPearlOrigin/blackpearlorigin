6bddcbf57c1a3d3838bd6f856d0031d19139b554
Davi Santos <brisolo32@outlook.com>
2022-12-16 09:48:35 -0300

    added some comments

--- src/panels/Browse.ts
+++ src/panels/Browse.ts
@@ -3,19 +3,17 @@ import { readTextFile } from "@tauri-apps/api/fs";
 import { invoke } from '@tauri-apps/api/tauri';
 
 export async function browse() {
+    // Read the file from AppData path
     const file = await readTextFile('temp/scrapers.json', {
         dir: BaseDirectory.AppLocalData
     })
 
+    // Parse the JSON and return it
     const json = JSON.parse(file)
-    console.log(file)
-
     return json
 }
 
 export async function search(title: string, path: string) {
-    // To be done...
-
     // Invoke the rust backend for initializing the scraper when a user presses the search button
     if (path.endsWith(".exe")) {
         invoke('handle_scraper', { scraper: 0 })

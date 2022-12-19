703544d6b510eedd0aa40829696ac3c580ce7006
zun1uwu <zuni0@protonmail.com>
2022-12-19 05:08:32 +0100

    minor changes

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -4,6 +4,6 @@
 
 <main class="container">
     <div class="main">
-        <h1>Welcome to Project Black Pearl <a class="underscore">|</a></h1>
+        <h1>Welcome to Project Black Pearl <a class="underscore">_</a></h1>
     </div>
 </main>

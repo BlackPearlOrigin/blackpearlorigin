e0aaf32f55b78e19d02eedf703fa19f12a67e268
Harsh Navdhare <harsh.navdhare@proton.me>
2022-12-19 13:49:15 +0530

    :bug: Use <b> instead of <a> to fix warning
    Signed-off-by: Harsh Navdhare <harsh.navdhare@proton.me>

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -4,6 +4,6 @@
 
 <main class="container">
     <div class="main">
-        <h1>Welcome to Project Black Pearl <a class="underscore">|</a></h1>
+        <h1>Welcome to Project Black Pearl <b class="underscore">|</b></h1>
     </div>
 </main>

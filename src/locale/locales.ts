24ab6b924c7beb2ae23303731df56a087f127053
Davi Santos <brisolo32@outlook.com>
2022-12-22 12:19:53 -0300

    Added finnish, estonian and arabic

--- src/locale/locales.ts
+++ src/locale/locales.ts
@@ -5,6 +5,9 @@ import eoLang from './lang/eo.json';
 import huLang from './lang/hu.json';
 import seLang from './lang/se.json';
 import frLang from './lang/fr.json';
+import eeLang from './lang/ee.json';
+import arLang from './lang/ar.json';
+import fiLang from './lang/fi.json';
 
 export default {
 	en: enLang,
@@ -13,9 +16,14 @@ export default {
 	eo: eoLang,
 	hu: huLang,
 	se: seLang,
-	fr: frLang
+	fr: frLang,
+	ee: eeLang,
+	ar: arLang,
+	fi: fiLang,
 };
 
 // credit to ohvii for swedish translation
 // credit to despair for french translation
 // credit to plaga for hungarian translation
+// credit to superweird7 for arabic translation
+// credit to goouckd for estonian and finnish translation

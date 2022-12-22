39c1636f5d0e06f8ad39ac1ee7eb818e296ef83c
zun1uwu <zuni0@protonmail.com>
2022-12-22 17:11:02 +0100

    Added polish

--- src/locale/locales.ts
+++ src/locale/locales.ts
@@ -5,6 +5,7 @@ import eoLang from './lang/eo.json';
 import huLang from './lang/hu.json';
 import seLang from './lang/se.json';
 import frLang from './lang/fr.json';
+import plLang from './lang/pl.json';
 
 export default {
 	en: enLang,
@@ -13,7 +14,8 @@ export default {
 	eo: eoLang,
 	hu: huLang,
 	se: seLang,
-	fr: frLang
+	fr: frLang,
+	pl: plLang
 };
 
 // credit to ohvii for swedish translation

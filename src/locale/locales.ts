017695c46a93c62eaa3ff2c609466806b8d8de5d
Davi Santos <brisolo32@outlook.com>
2022-12-24 09:17:01 -0300

    Added bulgarian

--- src/locale/locales.ts
+++ src/locale/locales.ts
@@ -20,6 +20,7 @@ import russianRU from './lang/ru.json';
 import serbianSR from './lang/sr.json';
 import turkishTR from './lang/tr.json';
 import romanianRO from './lang/ro.json';
+import bulgarianBG from './lang/bg.json';
 
 export default {
 	en: englishUS,
@@ -44,27 +45,30 @@ export default {
 	sr: serbianSR,
 	tr: turkishTR,
 	ro: romanianRO,
+	bg: bulgarianBG,
 };
 
 /*
 	Credits:
-		ohvii: 			     		Swedish translation
-		despair: 		     		French translation
-		plaga: 			     		Hungarian translation
-		superweird7: 	     		Arabic translation
-		GooUckd: 		     		Estonian and Finnish translation
-		Q99: 			     		Bosnian and Serbian translation
-		Mirza Čustović:      		Croatian translation
-		N3kowarrior:	     		Czech translation
-		Rafo:			     		Dutch translation
-		Brisolo32:		     		Portuguese (Brazil) and Esperanto translation
-		zun1:			     		German translation
-		xDal-Lio:		     		Italian translation
-		Lol123zv:		     		Latvian and Russian translation
+		ohvii: Swedish translation
+		despair: French translation
+		plaga: Hungarian translation
+		superweird7: Arabic translation
+		GooUckd: Estonian and Finnish translation
+		Q99: Bosnian and Serbian translation
+		Mirza Čustović: Croatian translation
+		N3kowarrior: Czech translation
+		Rafo: Dutch translation
+		Brisolo32: Portuguese (Brazil) and Esperanto translation
+		zun1: German translation
+		xDal-Lio: Italian translation
+		Lol123zv: Latvian and Russian translation
 		SteinScanner and Mr Mango:	Polish translation
-		Sup3r:			     		Portuguese (Portugal) translation
-		AlexanderMaxRanabel: 		Turkish translation
-		SoulStyle:					Romanian translation
-		TeeNam:						Vietnamese translation (To be finished)
-		Terrence Tingleberry:		Proofreading those translations
+		Sup3r: Portuguese (Portugal) translation
+		AlexanderMaxRanabel: Turkish translation
+		SoulStyle: Romanian translation
+		TeeNam:	Vietnamese translation (To be finished)
+    lyubomir501: Bulgarian translation
+
+		Terrence Tingleberry: Proofreading those translations
 */

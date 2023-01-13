c665f8190f8f752b84a1841b2ef0075bb1efbea5
Davi Santos <brisolo32@outlook.com>
2023-01-13 10:33:36 -0300

    Added routing for pages

--- src/Main.svelte
+++ src/Main.svelte
@@ -2,34 +2,17 @@
 	// Imports all pages and modules
 	import translations from './locale/locales';
 	import Browse from './components/Browse.svelte';
-	import Downloads from './components/Downloads.svelte';
-	import Home from './components/Home.svelte';
 	import Library from './components/Library.svelte';
 	import Preferences from './components/Preferences.svelte';
 	import { Modal } from 'svelte-simple-modal';
 	import { dict, t } from './locale/i18n';
 	import { loadLocale } from './scripts/Main';
+	import { Router, Link, Route } from 'svelte-navigator';
 
 	$: dict.set(translations);
 
 	// Loads the current locale
 	loadLocale();
-
-	var currentPage: number;
-
-	enum Pages {
-		Home = 0,
-		Browse = 2,
-		Library = 1,
-		Downloads = 3,
-		Prefs = 4,
-	}
-
-	// TS Function
-	// - Changes the currentPage variable to the number on args
-	function SwitchPage(Option: number) {
-		currentPage = Option;
-	}
 </script>
 
 <svelte:head>
@@ -42,54 +25,35 @@
 <!-- Only touch this file if adding a new page -->
 <!-- Or styling a Modal -->
 <!-- Otherwise, ignore it -->
-<main class="container">
-	<div class="sidenav">
-		<img src="icon.png" width="100" class="branding" alt="branding" />
-		<button class="menu-button" on:click="{() => SwitchPage(Pages.Home)}">
-			{$t('homeText')}
-		</button>
-		<button class="menu-button" on:click="{() => SwitchPage(Pages.Browse)}">
-			{$t('browseText')}
-		</button>
-		<button
-			class="menu-button"
-			on:click="{() => SwitchPage(Pages.Library)}"
-		>
-			{$t('libraryText')}
-		</button>
-		<!-- <button
-			class="menu-button"
-			on:click="{() => SwitchPage(Pages.Downloads)}"
-		>
-			{$t('downloadsText')}
-		</button> -->
-		<button class="menu-button" on:click="{() => SwitchPage(Pages.Prefs)}">
-			{$t('prefsText')}
-		</button>
-	</div>
+<Router>
+	<main class="container">
+		<div class="sidenav">
+			<img src="icon.png" width="100" class="branding" alt="branding" />
+			<Link class="menu-button" to="browse">{$t('browseText')}</Link>
+			<Link class="menu-button" to="/">{$t('libraryText')}</Link>
+			<Link class="menu-button" to="prefs">{$t('prefsText')}</Link>
+		</div>
+		<Route path="browse">
+			<Browse />
+		</Route>
 
-	{#if currentPage == 0}
-		<Home />
-	{:else if currentPage == 1}
-		<Modal
-			styleBg="{{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}"
-			styleWindow="{{
-				backgroundColor: '#000',
-				border: '1px solid #00ff00',
-				borderRadius: '0px',
-				float: 'center',
-			}}"
-			closeButton="{false}"
-		>
-			<Library />
-		</Modal>
-	{:else if currentPage == 2}
-		<Browse />
-	{:else if currentPage == 3}
-		<Downloads />
-	{:else if currentPage == 4}
-		<Preferences />
-	{:else}
-		<Home />
-	{/if}
-</main>
+		<Route path="/" primary="{false}">
+			<Modal
+				styleBg="{{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}"
+				styleWindow="{{
+					backgroundColor: '#000',
+					border: '1px solid #00ff00',
+					borderRadius: '0px',
+					float: 'center',
+				}}"
+				closeButton="{false}"
+			>
+				<Library />
+			</Modal>
+		</Route>
+
+		<Route path="prefs">
+			<Preferences />
+		</Route>
+	</main>
+</Router>

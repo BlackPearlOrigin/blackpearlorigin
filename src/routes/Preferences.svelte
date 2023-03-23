22066286b376b0128d168f9002addaa4b9965b15
Davi Santos <brisolo32@outlook.com>
2023-03-23 15:49:19 -0300

    Adds available plugins list to prefs page

--- src/routes/Preferences.svelte
+++ src/routes/Preferences.svelte
@@ -8,6 +8,9 @@
 		wipeLibrary,
 	} from '../scripts/Preferences';
 	import languageNames from '../locale/languages.json';
+	import { getScrapers } from '../scripts/Browse';
+
+	const scrapersList = getScrapers();
 
 	$: languages = Object.keys(translations);
 	$: dict.set(translations);
@@ -27,15 +30,27 @@
 					{$t('preferences.wipeLibrary')}</button
 				>
 			</div>
-			<label for="select">{$t('languageText')}:</label>
-			<div class="locale-settings">
-				<select bind:value="{$locale}">
-					{#each languages as languageCode, i}
-						<option value="{languageCode}"
-							>{languageNames[i]}</option
-						>
-					{/each}
-				</select>
+			<div class="availb-plugins">
+				<label for="available-plugins">Available plugins:</label>
+				<div class="available-plugins">
+					{#await scrapersList then scrapersList}
+						{#each scrapersList.scrapers as scraper}
+							<span>{scraper.name}</span>
+						{/each}
+					{/await}
+				</div>
+			</div>
+			<div class="lang-settings">
+				<label for="select">{$t('languageText')}:</label>
+				<div class="locale-settings">
+					<select bind:value="{$locale}">
+						{#each languages as languageCode, i}
+							<option value="{languageCode}"
+								>{languageNames[i]}</option
+							>
+						{/each}
+					</select>
+				</div>
 			</div>
 			<button class="save-button" on:click="{() => saveLangData($locale)}"
 				>{$t('preferences.saveText')}</button

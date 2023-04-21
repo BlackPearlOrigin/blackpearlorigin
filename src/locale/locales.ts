b1b8bab910492e2d923f46ec9cdcfd1517575b46
Harsh Navdhare <harsh.navdhare@proton.me>
2023-04-21 08:44:33 +0530

    Fix locale not found

--- src/locale/locales.ts
+++ src/locale/locales.ts
@@ -1,7 +1,6 @@
 import portugueseBR from './lang/pt-BR.json';
 import englishUS from './lang/en.json';
 import germanDE from './lang/de.json';
-import esperantoEO from './lang/eo.json';
 import arabicAR from './lang/ar.json';
 import croatianHR from './lang/hr.json';
 import bosnianBA from './lang/ba.json';
@@ -12,35 +11,9 @@ export default {
 	en: englishUS,
 	'pt-BR': portugueseBR,
 	de: germanDE,
-	eo: esperantoEO,
 	ar: arabicAR,
 	hr: croatianHR,
 	ba: bosnianBA,
 	sr: serbianSR,
 	lt: lithuaninanLT,
 };
-
-/*
-	Credits:
-		ohvii: Swedish translation
-		despair: French translation
-		plaga: Hungarian translation
-		superweird7: Arabic translation
-		GooUckd: Estonian and Finnish translation
-		Q99: Bosnian and Serbian translation
-		Mirza Čustović: Croatian translation
-		N3kowarrior: Czech translation
-		Rafo: Dutch translation
-		Brisolo32: Portuguese (Brazil) and Esperanto translation
-		zun1: German translation
-		xDal-Lio: Italian translation
-		Lol123zv: Latvian and Russian translation
-		SteinScanner and Mr Mango:	Polish translation
-		Sup3r: Portuguese (Portugal) translation
-		AlexanderMaxRanabel: Turkish translation
-		SoulStyle: Romanian translation
-		TeeNam:	Vietnamese translation (To be finished)
-    	lyubomir501: Bulgarian translation
-
-		Terrence Tingleberry: Proofreading those translations
-*/

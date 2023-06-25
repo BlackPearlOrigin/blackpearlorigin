d2b4454e38348cd66315f329102a3cc9452fc26a
Davi Santos <brisolo32@outlook.com>
2023-06-25 14:28:52 -0300

    Adds another toast indicating a reboot

--- src/routes/modals/Toast.svelte
+++ src/routes/modals/Toast.svelte
@@ -1,24 +1,36 @@
 <script lang="ts">
     import { log } from '../../scripts/Main';
     import '../../styles/Modal.scss';
-
+    import { t } from '../../locale/i18n';
     import { checkUpdate, installUpdate } from '@tauri-apps/api/updater';
     import { relaunch } from '@tauri-apps/api/process';
+    import { toast } from '@zerodevx/svelte-toast';
 
-    const update = async () => {
-        const { manifest } = await checkUpdate();
+    const update = async (toastText) => {
+        // const { manifest } = await checkUpdate();
 
-        log(
-            0,
-            `Installing update: ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`
-        );
+        // log(
+        //     0,
+        //     `Installing update: ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`
+        // );
 
-        await installUpdate();
-        await relaunch();
+        // await installUpdate();
+        toast.push(toastText, {
+            theme: {
+                '--toastBackground': '#171717',
+            },
+            duration: 5000,
+        });
+
+        setTimeout(async () => {
+            // await relaunch();
+        }, 5000);
     };
 </script>
 
 <div class="toast">
-    <p>New update available.</p>
-    <button on:click="{update}">Download</button>
+    <p>{$t('modals.updater.updateAvailable')}</p>
+    <button on:click="{() => update($t('modals.updater.reboot'))}"
+        >Download</button
+    >
 </div>

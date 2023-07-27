e7832d6cb88715c0f319e514b33f9fc2b3b69e04
Brisolo32 <brisolo32@outlook.com>
2023-07-27 18:46:59 -0300

    Removes useless old css variables

--- src/Main.svelte
+++ src/Main.svelte
@@ -52,56 +52,7 @@
 <!-- Only touch this file if adding a new page -->
 <!-- Or styling a Modal -->
 <!-- Otherwise, ignore it -->
-<body
-    style="
-		--shadow-color: rgba(0, 0, 0, 0.25);
-		--radius-round: 4px;
-		--radius-normal: 4px;
-		--font-color: #dddddd;
-		--global-background-color: #171717;
-		--sidebar-background-color: #101010;
-		--sidebar-font-weight: 300;
-		--sidebar-logo-color: #fff;
-		--settings-container-color: #101010;
-		--settings-container-button-color: #121212;
-		--settings-container-button-border-color: #323232;
-		--library-border-color: #323232;
-		--library-element-background: #121212;
-        --library-element-background-hovered: #181818;
-		--library-search-placeholder-color: #dddddd;
-		--library-search-font-color: #dddddd;
-		--library-name-font-weight: 400;
-		--library-game-modal-title-font-weight: 800;
-		--library-game-modal-desc-font-weight: 400;
-		--library-game-modal-run-button-background: #121212;
-		--library-game-modal-run-button-font-color: #dddddd;
-		--library-game-modal-run-button-font-weight: 900;
-		--library-game-modal-edit-button-color: #fff;
-		--library-game-modal-delete-button-color: #fff;
-		--library-game-modal-edit-delete-size: 2rem;
-		--library-game-modal-background-color: #101010;
-		--browse-border-color: #323232;
-		--browse-element-background: #121212;
-        --browse-element-background-hovered: #181818;
-		--browse-input-placeholder-color: #dddddd;
-		--browse-element-font-weight: 500;
-		--browse-game-border: #323232;
-		--browse-game-background-color: #0b0e10;
-		--browse-game-dl-border: #323232;
-		--browse-game-name-font-weight: 700;
-		--browse-game-dl-font-weight: 500;
-		--modal-background-color: #0b0d0e;
-		--modal-element-border-color: #323232;
-		--modal-element-background-color: #121212;
-		--modal-img-background-color: #0b0d0e;
-		--modal-img-border-color: #323232;
-		--modal-fetch-background-color: #0b0d0e;
-		--modal-fetch-font-weight: 500;
-		--modal-fetch-hover-color: #2d3739;
-		--modal-border-color: #323232;
-		--modal-placeholder-color: #fff;
-	"
->
+<body>
     <Router>
         <main class="container">
             <div class="sidenav">

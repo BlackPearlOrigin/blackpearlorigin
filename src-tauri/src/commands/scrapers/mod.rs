d8dd3db16cae0fffc66aff5c58074a5d195f9ca8
Brisolo32 <brisolo32@skiff.com>
2023-12-18 21:31:10 -0300

    Adds a basic implementation for plugin running
    Todo: fix parse_scrapers

--- src-tauri/src/commands/scrapers/mod.rs
+++ src-tauri/src/commands/scrapers/mod.rs
@@ -1,11 +1,9 @@
 pub mod rezi;
 pub type ToFrontend = Vec<Vec<Item>>;
 
-use std::thread;
-
 use crate::commands::scrapers::rezi::rezi_scraper;
-
 use super::logging::log_error;
+use rayon::prelude::*;
 
 #[derive(serde::Serialize)]
 pub struct Item {
@@ -20,7 +18,23 @@ struct Query {
     scrapers: Vec<String>
 }
 
-#[tauri::command]
-pub fn run_scraper(query: Query) {
-    // todo: add the code
+fn run_scraper(scraper_list: String) {
+    let scraper_deser: Query = serde_json::from_str(&scraper_list).unwrap();
+
+    let results = scraper_deser.scrapers
+        .par_iter()
+        .map(|scraper| {
+            parse_scrapers(scraper.clone(), scraper_deser.query.clone())
+        }); 
+}
+
+// todo: fix this code, my brain melted
+fn parse_scrapers(scraper: String, query: String) -> Vec<Item> {
+    match scraper.as_str() {
+        "Rezi" => rezi_scraper(query),
+        _ => {
+            log_error("Scraper not found");
+            Vec::new()
+        },
+    };
 }
